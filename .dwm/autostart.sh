d64830990db225b471f12ac07c610c5e9c06a69f
lutherush <velimir.baksa@gmail.com>
2020-03-30 09:55:57 +0200

    Your message about the commit

---
+++ .dwm/autostart.sh
@@ -0,0 +1,32 @@
+#! /bin/bash 
+
+dte(){
+  dte="$(date +"%A, %B %d | 🕒 %l:%M%p")"
+  echo -e "📅 $dte"
+}
+
+upd(){
+  upd=`checkupdates | wc -l`
+  echo -e "⟳ $upd updates"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e " $mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "💻 $cpu% cpu"
+}
+
+while true; do
+     xsetroot -name "$(cpu) | $(mem) | $(dte)"
+     sleep 10s    # Update time every ten seconds
+done &
+
